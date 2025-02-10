18bd05eeb3b4755d927016b62f93a1bce9255b72
roqeebYusuff <roqeebyusuff001@gmail.com>
2025-02-10 10:50:21 +0100

    ✨feat: Update ZonalVendorsSharedPage to change 'Approve' to 'Review' and open review modal on click

--- src/components/shared/zonal/index.tsx
+++ src/components/shared/zonal/index.tsx
@@ -202,10 +202,12 @@ function ZonalVendorsSharedPage() {
                                     <Dropdown.Item
                                       className="text-xs"
                                       onClick={() => {
-                                        console.log("Approve");
+                                        setReview(item);
+                                        setOpen(true);
                                       }}
                                     >
-                                      Approve
+                                      <PencilIcon className="mr-2 h-4 w-4" />
+                                      Review
                                     </Dropdown.Item>
                                   </div>
                                 )
