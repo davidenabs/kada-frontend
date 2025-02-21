2d041135a13737e711b31afc4483a8c0f1473d02
David Enabs <davidenabs@gmail.com>
2025-02-21 15:22:01 +0100

    ✨feat: Update base URL for production deployment; remove ngrok warning from API client

--- src/app/_api/client/index.ts
+++ src/app/_api/client/index.ts
@@ -6,7 +6,6 @@ const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     "Content-Type": "application/json",
-    "ngrok-skip-browser-warning": "true",
   },
 });
 
