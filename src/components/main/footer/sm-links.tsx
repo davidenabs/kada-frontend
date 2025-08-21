48ee435c4d6f9be2b70365cbe28eedcd474399aa
David Enabs <davidenabs@gmail.com>
2025-08-21 03:30:22 +0100

    Added kaduna state Agricultural Development Agency-KADA

--- src/components/main/footer/sm-links.tsx
+++ src/components/main/footer/sm-links.tsx
@@ -7,10 +7,11 @@ interface SocialMediaItem {
 }
 
 const socialMediaItems: SocialMediaItem[] = [
-  // {
-  //   icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/53676c140c391e938f1548d9d7b46197a5bb624846e7df24273588e1a2a02f4d?placeholderIfAbsent=true&apiKey=e3159558e3c24b7bb6f2db02f0873db3",
-  //   name: "kadakdsg",
-  // },
+  {
+    icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/53676c140c391e938f1548d9d7b46197a5bb624846e7df24273588e1a2a02f4d?placeholderIfAbsent=true&apiKey=e3159558e3c24b7bb6f2db02f0873db3",
+    name: "kaduna state Agricultural Development Agency-KADA",
+    link: "https://www.facebook.com/p/Kaduna-Agricultural-Development-Agency-KADA-61550900330127/"
+  },
   // {
   //   icon: "https://cdn.builder.io/api/v1/image/assets/TEMP/ad5a1c9867fe19ac0af95335ea666e70448c081215c8e128c87f7a39ff75455a?placeholderIfAbsent=true&apiKey=e3159558e3c24b7bb6f2db02f0873db3",
   //   name: "Kadakdsg",
@@ -24,7 +25,7 @@ const socialMediaItems: SocialMediaItem[] = [
 
 const SocialMediaLinks: React.FC = () => {
   return (
-    <div className="flex relative flex-col items-start py-5 pr-4 pl-5 mt-1.5 max-w-full text-base leading-tight rounded-lg bg-stone-900 bg-opacity-50 backdrop-blur-lg w-[279px] max-md:pr5 divide-y divide-white/65">
+    <div className="flex relative flex-col items-start py-5 pr-4 pl-5 mt-1.5 max-w-full text-base leading-tight rounded-lg bg-stone-900 bg-opacity-50 backdrop-blur-lg w-[279px] max-md:pr5 divide-y divide-white/65 text-wrap">
       <h3 className="font-bold pb-3">Follow us on social media</h3>
       {socialMediaItems.map((item, index) => (
         <a
@@ -37,7 +38,7 @@ const SocialMediaLinks: React.FC = () => {
             alt={`${item.name} icon`}
             className="object-contain shrink-0 w-6 aspect-square"
           />
-          <p className="my-auto">{item.name}</p>
+          <p className="my-auto text-wrap">{item.name}</p>
         </a>
       ))}
     </div>
