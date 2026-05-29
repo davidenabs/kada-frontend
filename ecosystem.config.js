1d6fcf2e1481a62b11960d2724a161455e237e51
David Enabs <davidenabs@gmail.com>
2026-05-29 09:13:34 +0100

    chore: update ecosystem config working directory path to project root

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "kada-front",
-      cwd: "/var/www/kada/frontend/kada-frontend",
+      cwd: "/var/www/kada/frontend",
       script: ".next/standalone/server.js",
       instances: 1,
       autorestart: true,
